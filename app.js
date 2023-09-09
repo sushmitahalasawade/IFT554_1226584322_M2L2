daec70a6703e46209b7329d1a6610f93a32ed591
Sushmita Halasawade <shalasaw@asu.edu>
2023-09-09 01:24:01 -0700

    done with Exercise 3: Constructors and DE structuring

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post("/calculate", (req, res) => {
   res.render("result", { sum, difference, product, quotient });
 });
 
-
+// Exercise 2. array and objects
 const books = [];
 // the get route for book page
 app.get("/book", (req, res) => {
@@ -41,6 +41,29 @@ app.post("/addBook", (req, res) => {
   const { title, author, publicationYear } = req.body;
   books.push({title,author,publicationYear});
   res.redirect("/book");
+});
+
+// Exercise 3. Constructors and DE structuring
+function User(name, age, email){
+  this.name = name;
+  this.age = age;
+  this.email=email;
+}
+const users = [];
+app.get("/user", (req, res) => {
+  userName="";
+  userAge="";
+  userEmail="";
+  res.render("userInfo",{users});
+});
+ // route handler
+ app.post("/createUser", (req, res) => {
+  const { name, age, email } = req.body;
+  const user = new User(name,age,email);
+  const{ name:userName,age:userAge,email:userEmail} = user;
+  users.push({userName,userAge,userEmail});
+  //res.render("userInfo",{userName,userAge,userEmail});
+  res.redirect("/user")
 });
 
 
